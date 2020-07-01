f367c86b878de58d8abf4a6c4d042b7fe9c701c5
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-07-01 18:03:55 +0800

    register using postgreSQL

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
-// const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : '0',
+      database : 'smart-brain'
+    }
+});
 
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(cors());
 
 const database = {
     users: [
@@ -26,33 +37,18 @@ const database = {
         entries: 0,
         joindate: new Date()
     }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
     ]
-
 }
 
 app.get('/', (req, res) => {
-    res.send(database.users)
+    res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-    // bcrypt.compare("apples", '$2a$10$NH50cIAjNnXNlWrjv4TRiu6RDm./i2qzReGb..SqyBV2Z3/8sXlzK', function(err, res) {
-    //     console.log('first guess', res)
-    // });
-    // bcrypt.compare("veggies", '$2a$10$NH50cIAjNnXNlWrjv4TRiu6RDm./i2qzReGb..SqyBV2Z3/8sXlzK', function(err, res) {
-    //     console.log('second guess', res)
-    // });
     if (req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password) {
-            res.json('success');
-        }
-    else {
+            res.json(database.users[0]);
+        } else {
            res.status(400).json('error logging in');
     }
 })
@@ -62,26 +58,23 @@ app.post('/register', (req, res) => {
     // bcrypt.hash(password, null, null, function(err, hash) {
     //     console.log(hash);
     // });
-    database.users.push({
-        id: '125',
+    db('users')
+    .returning('*')
+    .insert({
+        email:email,
         name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joindate: new Date()
+        joined: new Date(),
     })
-    res.json(database.users[database.users.length-1]);
+    .then(user => {
+        res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('unable to register!'))
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
     let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        }
-    })
+
     if (!found) {
         res.status(400).json('not found!');
     }
