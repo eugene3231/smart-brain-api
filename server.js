d006ab903244480ee8389d8714776e1226760148
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-07-01 18:13:55 +0800

    fix profile with db

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+const e = require('express');
 
 const db = knex({
     client: 'pg',
@@ -73,11 +74,16 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-
-    if (!found) {
-        res.status(400).json('not found!');
-    }
+    // let found = false;
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if (user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json('Not Found!')
+        }
+    })
+    .catch(err => res.status(400).json('Error getting user!'))
 })
 
 app.put('/image', (req, res) => {
