576e4bb4e0ed7fd0a03110c5d649705c76e1e904
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-07-03 16:06:30 +0800

    moved clarifai api to backend, disable empty form

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+// Clarifai API key
+const app = new Clarifai.App({
+    apiKey: '0513f64863ff45f7abfff9e22089b080'
+  });
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.status(400).json('Unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }
